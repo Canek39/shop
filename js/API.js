560046ab1d192d8154e9b992d10e7e31e3557db6
Canek39 <canek-boss@yandex.ru>
2021-07-07 23:11:09 +0200

    Create shop
    add js

---
+++ js/API.js
@@ -0,0 +1,13 @@
+export default {
+    fetch() {
+        return [
+            { title: 'Shirt', description: 'lorem', image: 'Rectangle_15.webp', price: 25, discount: 0 },
+            { title: 'Shoes', description: 'lorem', image: 'Image.webp', price: 35, discount: 1 },
+            { title: 'Skirt', description: 'lorem', image: 'Image_1.webp', price: 45, discount: 2 },
+            { title: 'Glass', description: 'lorem', image: 'Image_2.webp', price: 55, discount: 3 },
+            { title: 'Hat', description: 'lorem', image: 'Image_3.webp', price: 65, discount: 4 },
+            { title: 'Boots', description: 'lorem', image: 'Image_4.webp', price: 75, discount: 5 },
+            { title: 'Jeans', description: 'lorem', image: 'Rectangle_15.webp', price: 85, discount: 6 }
+        ]
+    }
+}
